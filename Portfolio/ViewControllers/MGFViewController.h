0fc7012a6350099c37a8acc85b3a9d11b9f2541e
Mark Feaver <mark.feaver@gmail.com>
2014-09-03 18:00:54 +1200

    Moved some files

---
+++ Portfolio/ViewControllers/MGFViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGFViewController.h
+//  Portfolio
+//
+//  Created by Mark Feaver on 3/09/14.
+//  Copyright (c) 2014 Mark Feaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGFViewController : UIViewController
+
+@end
---
+++ Portfolio/ViewControllers/MGFViewController.m
@@ -0,0 +1,29 @@
+//
+//  MGFViewController.m
+//  Portfolio
+//
+//  Created by Mark Feaver on 3/09/14.
+//  Copyright (c) 2014 Mark Feaver. All rights reserved.
+//
+
+#import "MGFViewController.h"
+
+@interface MGFViewController ()
+
+@end
+
+@implementation MGFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
