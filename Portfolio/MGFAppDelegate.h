baf1643acd6e63b0079763a7756561701ed3ba7a
Mark Feaver <mark.feaver@gmail.com>
2014-09-03 17:56:38 +1200

    Initial commit

---
+++ Portfolio/MGFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MGFAppDelegate.h
+//  Portfolio
+//
+//  Created by Mark Feaver on 3/09/14.
+//  Copyright (c) 2014 Mark Feaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Portfolio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Portfolio
+//
+//  Created by Mark Feaver on 3/09/14.
+//  Copyright (c) 2014 Mark Feaver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MGFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MGFAppDelegate class]));
+    }
+}
